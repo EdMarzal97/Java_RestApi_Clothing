10f3a041dce04277a72de4e71ae9ad04cb39cb88
edward4secret@hotmail.com <edward4secret@hotmail.com>
2022-07-16 18:45:29 -0500

    post completed

--- src/main/java/com/edmarzal/restapi/controller/ProductController.java
+++ src/main/java/com/edmarzal/restapi/controller/ProductController.java
@@ -1,12 +1,30 @@
 package com.edmarzal.restapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edmarzal.restapi.model.Product;
+import com.edmarzal.restapi.service.ProductService;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
     
     //TODO
     //This will CREATE a new Product on the database
+    @PostMapping("/product")
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+
+        Product savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+
+    } 
 
     //This will GET a Product by the ID
 
--- src/main/java/com/edmarzal/restapi/service/ProductService.java
+++ src/main/java/com/edmarzal/restapi/service/ProductService.java
@@ -1,5 +1,20 @@
 package com.edmarzal.restapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edmarzal.restapi.dao.ProductRepository;
+import com.edmarzal.restapi.model.Product;
+
+
+@Service
 public class ProductService {
     
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    } 
+
 }
